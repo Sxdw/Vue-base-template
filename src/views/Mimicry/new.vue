541b4e847d7c28442ccc3a72f6db908a0c1d8614
Sxdw <50639725@qq.com>
2020-09-21 13:58:07 +0800

    拟态化图标

---
+++ src/views/Mimicry/new.vue
@@ -0,0 +1,71 @@
+<template>
+<div>
+    <h3>拟态专区</h3>
+    <div class="cuuo">
+        <div class="drop-shadow">
+            <i class="el-icon-d-caret"></i>
+        </div>
+        <div class="drop-shadow">
+            <i class="el-icon-microphone"></i>
+        </div>
+        <div class="drop-shadow">
+            <i class="el-icon-watch-1"></i>
+        </div>
+        <div class="drop-shadow">
+            <i class="el-icon-truck"></i>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.cuuo {
+    display: flex;
+    background: #ecf0f3;
+}
+
+.drop-shadow {
+    padding: 10px;
+    margin: 20px;
+    width: 250px;
+    height: 250px;
+    border-radius: 20px;
+    box-shadow: 18px 18px 30px #d1d9e6, -18px -18px 30px #fff;
+}
+
+.el-icon-d-caret {
+    display: flex;
+    justify-content: center;
+    font-size: 200px;
+    margin-top: 20px;
+    color: #c5c5c5;
+}
+
+.el-icon-microphone {
+    display: flex;
+    justify-content: center;
+    font-size: 200px;
+    margin-top: 20px;
+    color: #c5c5c5;
+}
+
+.el-icon-watch-1 {
+    display: flex;
+    justify-content: center;
+    font-size: 200px;
+    margin-top: 20px;
+    color: #c5c5c5;
+}
+
+.el-icon-truck {
+    display: flex;
+    justify-content: center;
+    font-size: 200px;
+    margin-top: 20px;
+    color: #c5c5c5;
+}
+</style>
